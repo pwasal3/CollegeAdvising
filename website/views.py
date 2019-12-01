d5a0a0becc7c835b11d890349286f4e5291a81a9
Peter Wasala <pwasal3@illinois.edu>
2019-12-01 01:27:17 -0600

    added frontend to static files3

--- website/views.py
+++ website/views.py
@@ -28,10 +28,10 @@ def schoolApplied(request):
     return HttpResponse(render(request, 'SchoolsApplied.html'))
 
 def register(request):
-    return HttpResponse(render(request, 'static/register.html'))
+    return HttpResponse(render(request, '../static/register.html'))
 
 def login(request):
-    return HttpResponse(render(request, 'static/login.html'))
+    return HttpResponse(render(request, '../static/login.html'))
 
 def forgotPassword(request):
-    return HttpResponse(render(request, 'static/forgot-password.html'))
+    return HttpResponse(render(request, '../static/forgot-password.html'))
