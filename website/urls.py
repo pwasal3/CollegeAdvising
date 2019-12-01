67cd06cc811d3189b455771e848a9252889bbee7
Peter Wasala <pwasala3@illinois.edu>
2019-11-30 23:48:19 -0600

    static files

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('allschools', views.all_schools, name = 'all_schools'),
     path('updateSchoolName', views.updateSchoolName1, name = 'updateSchoolName'),
     path('restoreSchoolName', views.updateSchoolName2, name = 'restoreSchoolName')
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
