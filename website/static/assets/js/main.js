88291263d2fe0826643f843b1838fd3688f70870
Peter Wasala <pwasal3@illinois.edu>
2019-12-03 22:10:56 -0600

    firebase init

--- website/static/assets/js/main.js
+++ website/static/assets/js/main.js
@@ -3,7 +3,10 @@
 $(document).ready(function() {
 
   $('#searchSchoolsBtn').on('click', function() {
-    $.get( "search=0&inoutstate=0&state=is&tuition=0&enrollment=0&degree=0&gender=0", function( data ) {
+    $( "#searchResults" ).html( "<center> Loading </center>" );
+    stateCode = $('#stateInput').val();
+    console.log(stateCode);
+    $.get( "search=0&inoutstate=0&state=" + stateCode + "&tuition=0&enrollment=0&degree=0&gender=0", function( data ) {
       $( "#searchResults" ).html( data );
       console.log("loaded schools");
     });
