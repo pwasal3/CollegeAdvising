cda266716bab745aea53ef1b7133e2f8596d2c9d
Peter Wasala <pwasal3@illinois.edu>
2019-12-04 00:43:25 -0600

    firebase connected profile

---
+++ website/static/assets/js/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCVmT0l828BSUpMkOumuiTaMcZ6ioI88nE",
+    authDomain: "cs411collegeadvising.firebaseapp.com",
+    databaseURL: "https://cs411collegeadvising.firebaseio.com",
+    projectId: "cs411collegeadvising",
+    storageBucket: "cs411collegeadvising.appspot.com",
+    messagingSenderId: "795505184538",
+    appId: "1:795505184538:web:940b94324a1410064a26a3",
+    measurementId: "G-5XPWG51587"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
