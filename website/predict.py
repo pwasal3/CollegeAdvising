b5ab4cf3f052045255ca26a94579a49feea1a58e
Peter Wasala <pwasal3@illinois.edu>
2019-12-04 11:25:46 -0600

    Added more functionality to advanced function 1

--- website/predict.py
+++ website/predict.py
@@ -14,7 +14,7 @@ def filterByPrediction(optimismType, schools, profile):
     
     attributes_score = len(profile)
     act_score = 20 # default score if not specified
-    sat_score = 1000
+    sat_score = 20
     score = 0
     if 'SAT Score' in profile:
         sat_score = int(profile['SAT Score']) + attributes_score/2
@@ -24,12 +24,14 @@ def filterByPrediction(optimismType, schools, profile):
         for i in range(36):
             if sat_score < sat_high and sat_score > sat_low:
                 score = score - 1
+                break
             sat_high -= 30
             sat_low -= 30
         
 
     if 'ACT Score' in profile:
-        act_score = int(profile['ACT Score']) + attributes_score/2
+        act_score = int(profile['ACT Score']) 
+
         if(optimismType == 1):
             act_score -= 2
         elif(optimismType == 3):
@@ -37,11 +39,16 @@ def filterByPrediction(optimismType, schools, profile):
     
     print("number of schools before prediction", len(schools))
 
+    if act_score >= sat_score:
+        score = act_score + attributes_score/2
+    elif:
+        score = sat_score + attributes_score/2
+
     predictedSchools = []
     for school in schools:
         if school.averageACT == None:
             continue
-        if act_score >= school.averageACT:
+        if score >= school.averageACT:
             print("comparison", act_score, school.averageACT)
             predictedSchools.append(school)
             
