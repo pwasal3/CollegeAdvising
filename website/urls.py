28f21d48535e69ad0f071bf992758e0636668d10
Peter Wasala <pwasal3@illinois.edu>
2019-12-03 20:04:03 -0600

    added search 3

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('allschools', views.all_schools, name = 'all_schools'),
     path('updateSchoolName', views.updateSchoolName1, name = 'updateSchoolName'),
     path('restoreSchoolName', views.updateSchoolName2, name = 'restoreSchoolName'),
     path('myProfile', views.profile, name = 'profile'),
