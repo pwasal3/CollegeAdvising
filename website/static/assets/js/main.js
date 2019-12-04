955dad5c76ca06280bbc159d96075d1e7cdfb1e6
Peter Wasala <pwasal3@illinois.edu>
2019-12-03 22:04:16 -0600

    search states 2

--- website/static/assets/js/main.js
+++ website/static/assets/js/main.js
@@ -3,7 +3,8 @@
 $(document).ready(function() {
 
   $('#searchSchoolsBtn').on('click', function() {
-    $.get( "search=0&inoutstate=0&state=is&tuition=0&enrollment=0&degree=0&gender=0", function( data ) {
+    stateCode = $('#stateInput').val();
+    $.get( "search=0&inoutstate=0&state=" + stateCode + "&tuition=0&enrollment=0&degree=0&gender=0", function( data ) {
       $( "#searchResults" ).html( data );
       console.log("loaded schools");
     });
