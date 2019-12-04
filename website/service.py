e712f55f7270796761f406c118d4e68771ffa6e2
Peter Wasala <pwasal3@illinois.edu>
2019-12-04 01:27:38 -0600

    added column

--- website/service.py
+++ website/service.py
@@ -4,19 +4,17 @@ from django.contrib.auth.hashers import make_password
 from website.models import Schools, User
 
 def getSchools(searchType, inorout, state, tuition, size, degree, gender):
-    print("starting query")
     query = "SELECT * FROM CollegeData WHERE "
     if(state != "--"):
         query += "state = '{0}' ".format(state)
     else:
         query += "state <> '{0}' ".format("ZZ")
-    print("inorout", inorout)
 
     if(inorout == 0):
         query += "AND tutionInState < {0} ".format(tuition)
     elif(inorout == 1):
         query += "AND tutionOutState < {0} ".format(tuition)
-    print(query)
+
     query += "AND size < {0} ".format(size)
 
     if(degree == 1):
@@ -33,7 +31,6 @@ def getSchools(searchType, inorout, state, tuition, size, degree, gender):
     elif(gender == 1):
         query += "AND menOnly = 0 AND womenOnly = 1"
     
-    print(query)
     schoolsIn = Schools.objects.raw(query)
     return list(schoolsIn)
 
--- website/static/assets/js/main.js
+++ website/static/assets/js/main.js
@@ -2,10 +2,89 @@
 
 $(document).ready(function() {
   
-  $('#searchSchoolsBtn').on('click', function() {
-    
-
+  $('#searchSchools').on('click', function() {
     var url = "search=0";
+
+    var inOutState = $('#inOutInput').val();
+    url += "&inoutstate=" + inOutState;
+
+    var stateCode = $('#stateInput').val();
+    url += "&state=" + stateCode;
+
+    var tuitionCode = $('#tuitionInput').val();
+    url += "&tuition=" + tuitionCode;
+
+    var enrollmentCode = $('#sizeInput').val();
+    url += "&enrollment=" + enrollmentCode;
+
+    var degreeCode = $('#degreeInput').val();
+    url += "&degree=" + degreeCode;
+
+    var genderCode = $('#genderInput').val();
+    url += "&gender=" + genderCode;
+
+    $.get( url, function( data ) {
+      $( "#searchResults" ).html( data );
+      console.log("loaded schools");
+    });
+  });
+
+  $('#searchSafety').on('click', function() {
+    var url = "search=1";
+
+    var inOutState = $('#inOutInput').val();
+    url += "&inoutstate=" + inOutState;
+
+    var stateCode = $('#stateInput').val();
+    url += "&state=" + stateCode;
+
+    var tuitionCode = $('#tuitionInput').val();
+    url += "&tuition=" + tuitionCode;
+
+    var enrollmentCode = $('#sizeInput').val();
+    url += "&enrollment=" + enrollmentCode;
+
+    var degreeCode = $('#degreeInput').val();
+    url += "&degree=" + degreeCode;
+
+    var genderCode = $('#genderInput').val();
+    url += "&gender=" + genderCode;
+
+    $.get( url, function( data ) {
+      $( "#searchResults" ).html( data );
+      console.log("loaded schools");
+    });
+  });
+
+  $('#searchMatch').on('click', function() {
+    var url = "search=2";
+
+    var inOutState = $('#inOutInput').val();
+    url += "&inoutstate=" + inOutState;
+
+    var stateCode = $('#stateInput').val();
+    url += "&state=" + stateCode;
+
+    var tuitionCode = $('#tuitionInput').val();
+    url += "&tuition=" + tuitionCode;
+
+    var enrollmentCode = $('#sizeInput').val();
+    url += "&enrollment=" + enrollmentCode;
+
+    var degreeCode = $('#degreeInput').val();
+    url += "&degree=" + degreeCode;
+
+    var genderCode = $('#genderInput').val();
+    url += "&gender=" + genderCode;
+
+    $.get( url, function( data ) {
+      $( "#searchResults" ).html( data );
+      console.log("loaded schools");
+    });
+  });
+
+  $('#searchReach').on('click', function() {
+    var url = "search=3";
 
     var inOutState = $('#inOutInput').val();
     url += "&inoutstate=" + inOutState;
